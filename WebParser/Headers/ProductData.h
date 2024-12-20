83bacf5241bcae71eefcc7718383c1f1458cb0ec
Dmitry Barkovets <berkut_998@mail.ru>
2024-12-20 19:45:26 +0300

    added operator == != << for class ProductData

--- WebParser/Headers/ProductData.h
+++ WebParser/Headers/ProductData.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <iostream>
+
 namespace WP
 {
 	class ProductData final
@@ -16,6 +18,32 @@ namespace WP
 		std::string manufacturer;
 		int category_id = 0;
 		bool stock_status = true;
+
+
+		bool operator!=(const ProductData& other) const
+		{
+			if (name != other.name || url != other.url || pathToImage != other.pathToImage ||
+				price != other.price || description != other.description || category != other.category ||
+				image_url != other.image_url || product_id != other.product_id || manufacturer != other.manufacturer ||
+				category_id != other.category_id || stock_status != other.stock_status)
+				return true;
+			return false;
+		}
+		bool operator==(const ProductData& other) const
+		{
+			if (name == other.name && url == other.url && pathToImage == other.pathToImage &&
+				price == other.price && description == other.description && category == other.category &&
+				image_url == other.image_url && product_id == other.product_id && manufacturer == other.manufacturer &&
+				category_id == other.category_id && stock_status == other.stock_status)
+				return true;
+			return false;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const ProductData& pd)
+		{
+			os << pd.product_id;
+			return os;
+		}
 	};
 }
 
